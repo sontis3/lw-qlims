899e0504a0a54b1ca38f14f143323b4541fad140
sontis3 <oleg.ovcharenko@labworks.ru>
2018-08-09 19:33:04 +0300

    get customers from rest API

--- src/components/directory/customers.vue
+++ src/components/directory/customers.vue
@@ -15,28 +15,53 @@ export default {
   data: () => ({
     columns: [
       {
-        name: 'desc', required: true, label: 'Dessert (100g serving)', align: 'left', field: 'name', sortable: true
+        name: 'desc',
+        required: true,
+        label: 'Dessert (100g serving)',
+        align: 'left',
+        field: 'name',
+        sortable: true
       },
       {
-        name: 'calories', label: 'Calories', field: 'calories', sortable: true
+        name: 'calories',
+        label: 'Calories',
+        field: 'calories',
+        sortable: true
       },
       {
-        name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true
+        name: 'fat',
+        label: 'Fat (g)',
+        field: 'fat',
+        sortable: true
       },
       {
-        name: 'carbs', label: 'Carbs (g)', field: 'carbs'
+        name: 'carbs',
+        label: 'Carbs (g)',
+        field: 'carbs'
       },
       {
-        name: 'protein', label: 'Protein (g)', field: 'protein'
+        name: 'protein',
+        label: 'Protein (g)',
+        field: 'protein'
       },
       {
-        name: 'sodium', label: 'Sodium (mg)', field: 'sodium'
+        name: 'sodium',
+        label: 'Sodium (mg)',
+        field: 'sodium'
       },
       {
-        name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        name: 'calcium',
+        label: 'Calcium (%)',
+        field: 'calcium',
+        sortable: true,
+        sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
       },
       {
-        name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        name: 'iron',
+        label: 'Iron (%)',
+        field: 'iron',
+        sortable: true,
+        sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
       }
     ],
     tData: [
@@ -60,12 +85,45 @@ export default {
         calcium: '14%',
         iron: '12%'
       }
-
     ]
-  })
+  }),
+
+  // хук когда компонент загружен
+  mounted() {
+    this.fetchCustomers();
+  },
+  methods: {
+    async fetchCustomers() {
+      return this.$axios({
+        method: 'get',
+        url: 'http://localhost:3000/api/dir/customers'
+      })
+        .then((response) => {
+          this.columns = [
+            {
+              name: 'name',
+              required: true,
+              label: 'Наименование заказчика',
+              align: 'left',
+              field: 'name',
+              sortable: true
+            },
+            {
+              name: 'active',
+              required: true,
+              label: 'Действующий',
+              align: 'left',
+              field: 'active',
+              sortable: true
+            }
+          ];
+          this.tData = response.data;
+        })
+        .catch();
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
